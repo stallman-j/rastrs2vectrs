326650ea8da7bce9ed83b5949b2b310fc184547c
stallman-j <jillian.stallman@gmail.com>
2024-04-07 23:48:08 -0400

    fixed functions for create_long_run_vars and collapse_panel_to_annual in cleaning functions

--- R/cleaning_functions.R
+++ R/cleaning_functions.R
@@ -1,3 +1,66 @@
+#' Description: Create Long-run variables
+#' @description With a df with a time dimension, create a bunch of long-run variables
+#' @param panel_df panel data frame
+#' @param id_varname the variable name of the
+#' @param time_varname variable name of the largest
+#' @param variable_to_manipulate character vector of the variable that you want long-run values for
+#' @param variable_namestub character vector, the namestub that you want appended to the long-run varnames
+#' @param out_path output file.path() to where to save the final data frame. Default is NULL, in which case the df isn't saved
+#' @param output_filename filename to save the final data frame, if you provide an out_path. Default NULL doesn't do anything
+#' @returns a data frame that for the variable_to_manipulate
+#' @examples
+#' # example code
+#'
+#' @export
+create_long_run_vars <- function(panel_df,
+                                 id_varname   = "DHSID",
+                                 time_varname = "year",
+                                 variable_to_manipulate = "precip_annual_mean",
+                                 variable_namestub      = "precip",
+                                 out_path = NULL,
+                                 output_filename = NULL
+
+){
+  # create some variables to manipulate; this is easier in base R than dplyr?
+  panel_df[["time_var"]]          <- panel_df[[time_varname]]
+  panel_df[["var_to_manipulate"]] <- panel_df[[variable_to_manipulate]]
+  panel_df[["id_var"]]            <- panel_df[[id_varname]]
+
+  panel_df_tmp <- panel_df %>%
+    dplyr::filter(!is.na(time_var)) %>%
+    dplyr::group_by(time_var)%>%
+    dplyr::arrange(id_varname) %>% # sorted by ID, chronologically within
+    dplyr::ungroup() %>%
+    dplyr::group_by(id_var) %>%
+    dplyr::mutate(lr_avg = mean(var_to_manipulate),
+                  lr_sd  = sd(var_to_manipulate),
+                  lr_zscore = (var_to_manipulate - mean(var_to_manipulate))/(sd(var_to_manipulate))) %>%
+    dplyr::ungroup()
+
+
+  # change the variable names
+  for (varname in c("lr_avg","lr_sd","lr_zscore")) {
+    names(panel_df_tmp)[names(panel_df_tmp)== varname] <- paste0(variable_namestub, "_",varname)
+
+  }
+
+  # take out the temp vars we made to make the code easy
+  panel_df_out <- panel_df_tmp %>%
+    dplyr::select(-c(time_var,var_to_manipulate,id_var))
+
+  if (!is.null(out_path) & !is.null(out_filename)){
+  if (!dir.exists(out_path)) dir.create(out_path, recursive = TRUE) # recursive lets you create any needed subdirectories
+
+  saveRDS(panel_df_out,
+          file = file.path(out_path,
+                           out_filename))
+
+  }
+  rm(panel_df_tmp)
+
+  return(panel_df_out)
+}
+
 #' Break up a thing into chunks of a certain size n
 #' @param x a vector or list, things you want to split up
 #' @param n number of chunks
--- R/collapse_panel_to_annual.R
+++ R/collapse_panel_to_annual.R
@@ -6,8 +6,8 @@
 #' @param value_varname_old the current name of the values you're using for the values column
 #' @param value_varname_new new variable name for the values you're interested in summarizing from monthly to annually
 #' @param id_varname the observation ID. default is "vector_cast_id" because that's what raster_extract_to_long_df outputs as the id, the observation ID
-#' @param out_path the file.path() to where you would like to save the annual df. Gets created if it doesn't already exist. Defaults to working directory
-#' @param out_filename The name you would like the long annual panel to be saved as. defaults to "annualized_df.rds"
+#' @param out_path the file.path() to where you would like to save the annual df. Gets created if it doesn't already exist. Defaults to NULL in which case the outputted df is not saved anywhere
+#' @param out_filename The name you would like the long annual panel to be saved as. default of NULL means that the annual df won't get saved
 #' @param merge_df if you'd like to merge on another DF to this annual panel, input here the df. This will do left_join(annual_temp_df,merge_df). Default NULL which is do not merge any other DFs on here. Assumes you're just merging by the id_varname which is present in both or something equally obvious that dplyr can recognize
 #' @returns an annualized df with 3 annual rolling averages
 #' @examples
@@ -19,12 +19,11 @@ collapse_panel_to_annual <- function(in_df,
                                      value_varname_old  = "precip_mean",
                                      value_varname_new  = "precip",
                                      id_varname    = "vector_cast_id",
-                                     out_path      = getwd(), #file.path(data_external_clean,"merged","DHS_ERA5","annual")
-                                     out_filename  = "annualized_df.rds",
+                                     out_path      = NULL, #file.path(data_external_clean,"merged","DHS_ERA5","annual")
+                                     out_filename  = NULL,
                                      merge_df      = NULL
 ){
 
-  if (!dir.exists(out_path)) dir.create(out_path, recursive = TRUE) # recursive lets you create any needed subdirectories
   # for converting character varnames to grouping vars
   # https://stackoverflow.com/questions/52437463/function-calling-variable-names-for-group-by-in-dplyr-how-do-i-vectorise-this
 
@@ -54,10 +53,15 @@ collapse_panel_to_annual <- function(in_df,
     annual_temp_df <- dplyr::left_join(annual_temp_df,merge_df)
   }
 
+  if (!is.null(out_path) & !is.null(out_filename)){
+    if (!dir.exists(out_path)) dir.create(out_path, recursive = TRUE) # recursive lets you create any needed subdirectories
 
-  saveRDS(annual_temp_df,
-          file = file.path(out_path,
-                           out_filename))
+    saveRDS(annual_temp_df,
+            file = file.path(out_path,
+                             out_filename))
+
+  }
+
 
   return(annual_temp_df)
 
